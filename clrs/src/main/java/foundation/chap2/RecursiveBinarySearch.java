e6e2b3ac2a07a1b62db1b3c7b0e4253e89eff221
Ritesh Dalvi <riteshdalvi270@gmail.com>
2017-06-20 21:34:57 -0500

    Iterative and recursive binary search approach.

---
+++ clrs/src/main/java/foundation/chap2/RecursiveBinarySearch.java
@@ -0,0 +1,47 @@
+package foundation.chap2;
+
+/**
+ * Recursive binary search to search for an element.
+ * Created by ritesh on 6/20/17.
+ */
+public class RecursiveBinarySearch {
+
+    public static void main(String args[]) {
+
+        final int[] input = new int[] {1,2,3,5,49,51};
+
+        int index = recursiveBinarySearch(input,0,input.length-1,2);
+
+        System.out.println(index);
+    }
+
+    /**
+     * Complexity is the number of sub-problems it is divided into
+     * T(n) = aT(n/b) + O(1) [combine] =  O(logn)
+     */
+    public static int recursiveBinarySearch(final int[] in,int p, int r, int key) {
+
+        if(p>r) {
+            return -1;
+        }
+
+        int q = (p+r)/2;
+
+        int element = in[q];
+
+        if(key<element) {
+
+            return recursiveBinarySearch(in,p,q,key);
+        }
+
+        if(key>element) {
+            return recursiveBinarySearch(in,q+1,r,key);
+        }
+
+        if(key == element) {
+            return q;
+        }
+
+        return -1;
+    }
+}
