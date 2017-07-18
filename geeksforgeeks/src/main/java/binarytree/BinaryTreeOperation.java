bb86e736146c72b05cb9646e3b9cc5c4e11b7129
Ritesh Dalvi <riteshdalvi270@gmail.com>
2017-07-18 01:31:10 -0500

    Detect cycle with rank, binary tree operation

---
+++ geeksforgeeks/src/main/java/binarytree/BinaryTreeOperation.java
@@ -0,0 +1,7 @@
+package binarytree;
+
+/**
+ * Created by ritesh on 7/18/17.
+ */
+public class BinaryTreeOperation {
+}
--- geeksforgeeks/src/main/java/graph/DetectCycleUndirectedGraphUnionRank.java
+++ geeksforgeeks/src/main/java/graph/DetectCycleUndirectedGraphUnionRank.java
@@ -8,7 +8,68 @@ public class DetectCycleUndirectedGraphUnionRank
 
     public static void main(String args[])
     {
+        int V = 5;
+        int E = 5;
 
+        final Graph graph = new Graph(V, E);
+
+        if(isCycle(graph)) {
+            System.out.println("Cycle detected");
+        }else {
+            System.out.println("Cycle not detected");
+        }
+    }
+
+    private static boolean isCycle(final Graph graph) {
+
+        final Subset subset[] = new Subset[graph.V];
+
+        for(int i=0; i<graph.V;i++) {
+            subset[i].parent = i;
+            subset[i].rank = 0;
+        }
+
+
+        for(int e=0; e<graph.E-1;e++) {
+
+           final Edge edge = graph.edges[e];
+
+           int x = find(subset,edge.src);
+           int y = find(subset,edge.destination);
+
+           if(x == y) {
+               return true;
+           }
+
+           union(subset,edge.src, edge.destination);
+        }
+
+        return false;
+    }
+
+    private static int find(Subset[] subset, int src) {
+
+        if(subset[src].parent == src) {
+            return src;
+        }
+
+        return find(subset,subset[src].parent);
+    }
+
+
+    private static void union(Subset[] subsets,int src, int destination) {
+
+        int srcparent = find(subsets,src);
+        int destparent = find(subsets,destination);
+
+        if(subsets[srcparent].rank > subsets[destination].rank) {
+            subsets[destination].parent = subsets[srcparent].parent;
+        }else if(subsets[srcparent].rank < subsets[destination].rank) {
+            subsets[srcparent].parent = subsets[destination].parent;
+        }else if(subsets[srcparent].rank == subsets[destparent].rank) {
+            subsets[destination].parent = subsets[srcparent].parent;
+            subsets[srcparent].rank++;
+        }
     }
 
     static class Graph
@@ -17,14 +78,27 @@ public class DetectCycleUndirectedGraphUnionRank
         int V;
         int E;
 
-        Edge[] egdes;
+        Edge[] edges;
+
+        Graph(int V, int E) {
+            this.V = V;
+            this.E = E;
+
+            for(int i=0; i<E;i++) {
+                edges[i] = new Edge();
+            }
+        }
     }
 
-    static class Edge
+    static class Edge implements Comparable<Edge>
     {
-
         int src;
         int destination;
+
+        @Override
+        public int compareTo(Edge o) {
+            return 0;
+        }
     }
 
     class Subset
